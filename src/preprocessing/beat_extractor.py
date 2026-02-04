8f4a7ea2912855f226b0bd9d93449e48b8291c50
fodilayadi16-coder <fodil.ayadi16@gmail.com>
2026-02-04 19:04:44 +0100

    Updated beat and rhythm extractor

--- src/preprocessing/beat_extractor.py
+++ src/preprocessing/beat_extractor.py
@@ -5,19 +5,29 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..')) # include the 
 
 from loaders.beat_rec_finder import get_all_records
 from loaders.beat_wfdb_loader import load_record
+from preprocessing.rr_intervals import plot_rr_distribution
 from preprocessing.label_maps import beat_label_map
 
 
 # ----------------------------------------------- Extract Beats from All Records -----------------------------------------------
 
-def extract_beats(records, window_size=180):  # 0.5 seconds segmented window (generally RR interval = 0.6 to 1 second)
+def extract_beats(records, window_size=180, collect_rr=True):  # 0.5 seconds segmented window (generally RR interval = 0.6 to 1 second)
     half_window = window_size // 2
     X = []              # input features: beat signals (not suitable for CNN models)
     y = []              # labels
+    rr_intervals_all = []
 
     for rec_path in records:
         try:
             signal, r_peaks, symbols, fs = load_record(rec_path)
+
+                # ---------- RR INTERVAL COLLECTION ----------
+
+            if collect_rr and len(r_peaks) > 1:
+                rr = np.diff(r_peaks) / fs  # seconds
+                rr_intervals_all.extend(rr)
+
+                # ---------- BEAT EXTRACTION ----------
 
             for i, r in enumerate(r_peaks):
                 if symbols[i] not in beat_label_map:
@@ -32,8 +42,9 @@ def extract_beats(records, window_size=180):  # 0.5 seconds segmented window (ge
 
     X = np.array(X).reshape(-1, window_size, 1) # 3D array == Tensor of beat signals (shape: (num_beats, 187, 1))
     y = np.array(y)                             # 1D array of shape (beat_labels,)  .eg (1000,)
+    rr_intervals_all = np.array(rr_intervals_all)                        
 
-    return X, y
+    return X, y, rr_intervals_all
 
 
 # ----------------------------------------------- Main Script -----------------------------------------------
@@ -42,7 +53,7 @@ if __name__ == "__main__":
     records = get_all_records("data/raw")
     print("Total records found:", len(records))
 
-    X, y = extract_beats(records)
+    X, y, rr_intervals  = extract_beats(records)
     print("Beats extracted:", X.shape[0])
 
     # Save processed dataset
@@ -51,8 +62,5 @@ if __name__ == "__main__":
     np.save("data/processed/beat_y.npy", y)
 
     print("Saved beat dataset to data/processed/")
-    print("X shape:", X.shape)
-    print("y shape:", y.shape)
-    print("Sample records:", records[:5])
-    print("Class distribution:", np.unique(y, return_counts=True))
-
+ 
+    plot_rr_distribution(rr_intervals) # Normal RR intervals: 0.6-1s
